e0f8757454e8ddf8d16ebde8591b9b978813bec9
Burhan Cikili <35443804+bcikili@users.noreply.github.com>
2023-05-11 11:46:07 -0500

    bonus part added into branch

--- dictionary_hw/hw1.py
+++ dictionary_hw/hw1.py
@@ -3,3 +3,7 @@ import string
 import os 
 
 print('Hello, this is a dictionary program.')
+
+print('_________________________________________________________')
+
+print("Homework bonus part added into this program.")
